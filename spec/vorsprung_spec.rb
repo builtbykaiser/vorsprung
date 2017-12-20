4d28a75f57be9f284ef999166853714f21c25b04
Sergio Pantoja <2530982+sergiopantoja@users.noreply.github.com>
2017-12-20 18:34:36 -0500

    Add test for Rails version

--- spec/vorsprung_spec.rb
+++ spec/vorsprung_spec.rb
@@ -1,5 +1,39 @@
 RSpec.describe Vorsprung do
+  before(:all) { setup }
+  after(:all)  { teardown }
+
   it "has a version number" do
     expect(Vorsprung::VERSION).not_to be nil
+  end
+
+  it "uses the proper version of Rails" do
+    Dir.chdir(app_path) do
+      expect(`rails --version`).to include Vorsprung::RAILS_VERSION
+    end
+  end
+
+  private
+
+  def temp_dir
+    "tmp"
+  end
+
+  def app_name
+    "myapp"
+  end
+
+  def app_path
+    "#{temp_dir}/#{app_name}"
+  end
+
+  def setup
+    FileUtils.mkdir_p(temp_dir)
+    Dir.chdir(temp_dir) do
+      Bundler.clean_system("vorsprung new #{app_name}")
+    end
+  end
+
+  def teardown
+    FileUtils.rm_rf(temp_dir)
   end
 end
