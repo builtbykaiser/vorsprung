453f3e1a3e4dc77deef31cf02d122f7aefeec998
Sergio Pantoja <2530982+sergiopantoja@users.noreply.github.com>
2017-12-20 19:32:29 -0500

    Hide output when creating new Rails app
    Cleaner output in tests...

--- lib/vorsprung/cli.rb
+++ lib/vorsprung/cli.rb
@@ -3,7 +3,7 @@ module Vorsprung
     desc "new APP_NAME", "create a new Rails app called APP_NAME"
     def new(app_name)
       puts "Vorsprinkling you a new Rails app..."
-      Bundler.clean_system("rails new #{app_name}")
+      Bundler.clean_system("rails new #{app_name} > /dev/null")
     end
   end
 end
